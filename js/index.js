1ba8239e648e769cecea0933807bc4044e6305da
DESKTOP-QOUBLIC\hsh86 <hsh8616@naver.com>
2018-02-19 17:51:42 +0900

    CHAGNE file upload UI

--- js/index.js
+++ js/index.js
@@ -168,7 +168,7 @@ let deleteDocument = () => {
             data: data,
             dataType: 'html',
             success: function(data) {
-                alert("삭제가 완료되었습니다!");
+                alert("삭제가 완료되었습니다.");
                 getDocumentList(c3code);
             },
             error: function(request, status, error) {
@@ -253,14 +253,28 @@ $("#upload").submit(function(e) {
     if(c3code !== "" && filecnt !== 0){
         formData.append('c3_code', c3code);
 
+        //TODO : li -> status -> i = spinner
+        let status = document.createElement("i");
+        status.setAttribute("class", "fas fa-spinner fa-pulse");
+        status.style["margin-top"] = "8px";
+
+        $(".status").append(status);
+
         $.ajax({
             url: 'http://igrus.mireene.com/php/dms_php/savefile.php',
             type: 'POST',
             data: formData,
             success: function (data) {
-                alert(data);
-                document.getElementById("messages").innerHTML = "";
-                document.getElementById("upload").reset();
+                let dataStatus = data.split("\n");
+                for(let i=0; i<dataStatus.length-1; i++){
+                    if (dataStatus[i] === "존재하는 이름입니다.") {
+                        $("#fileindex"+i).addClass("danger");
+                        $("#fileindex"+i+" .status i").switchClass("fas fa-spinner fa-pulse", "fas fa-times", 1000, "easeInOutQuad");
+                    }
+                }
+
+                //TODO : data에 따라 check / times i 추가
+
                 return false;
             },
             cache: false,
@@ -283,14 +297,6 @@ function $id(id) {
     return document.getElementById(id);
 }
 
-//
-// output information
-function Output(msg) {
-    var m = $id("messages");
-    m.innerHTML = msg + m.innerHTML;
-}
-
-
 // call initialization file
 if (window.File && window.FileList && window.FileReader) {
     Init();
@@ -301,20 +307,19 @@ if (window.File && window.FileList && window.FileReader) {
 function Init() {
 
     var fileselect = $id("fileselect");
+    var filedrag = $id("filedrag");
 
     // file select
     fileselect.addEventListener("change", FileSelectHandler, false);
 
-    // // is XHR2 available?
-    // var xhr = new XMLHttpRequest();
-    // if (xhr.upload) {    
-    //     // file drop
-    //     filedrag.addEventListener("dragover", FileDragHover, false);
-    //     filedrag.addEventListener("dragleave", FileDragHover, false);
-    //     filedrag.addEventListener("drop", FileSelectHandler, false);
-    //     filedrag.style.display = "block";
-
-    // }
+    // is XHR2 available?
+    var xhr = new XMLHttpRequest();
+    if (xhr.upload) {    
+        // file drop
+        filedrag.addEventListener("dragover", FileDragHover, false);
+        filedrag.addEventListener("dragleave", FileDragHover, false);
+        filedrag.addEventListener("drop", FileSelectHandler, false);
+    }
 
 }
 
@@ -340,16 +345,63 @@ function FileSelectHandler(e) {
     for (var i = 0, f; f = files[i]; i++) {
         ParseFile(f, i);
     }
-
 }
 
 
-function ParseFile(file, index) {
-    Output(
-        "<p>File info: <strong>" + file.name +
-        "</strong> type: <strong>" + file.type +
-        "</strong> size: <strong>" + file.size +
-        "</strong> bytes </p>"
-    );    
+let ParseFile = (file, index) => {
+    let filelist = $id("filelistul");
+
+    let li = document.createElement("li");
+    li.setAttribute("class", "row");
+    li.setAttribute("id", "fileindex"+index);
+
+    let div1 = document.createElement("div");
+    div1.setAttribute("class", "col-md-2");
+
+    let circle = document.createElement("i");
+    circle.setAttribute("class", "far fa-circle fa-2x");
+    circle.style["margin-top"] = "8px";
+
+    let circlenumber = document.createElement("span");
+    circlenumber.setAttribute("class", "fa-layers-text fa-inverse");
+    circlenumber.setAttribute("data-fa-transform", "left-2 down-1");
+    circlenumber.style["font-weight"] = "900";
+    circlenumber.style["color"] = "#555";
+    circlenumber.textContent = filelist.getElementsByTagName("li").length + 1;
+
+    div1.appendChild(circle);
+    div1.appendChild(circlenumber);
+    li.appendChild(div1);
+
+    let div2 = document.createElement("div");
+    div2.setAttribute("class", "col-md-8");
+
+    let filename = document.createElement("p");
+    filename.setAttribute("class", "filename");
+    filename.textContent = file.name;
+
+    if(file.name.length > 22){
+        filename.textContent = file.name.substr(0,19) + "…";
+    }
+
+    let filesize = document.createElement("p");
+    filesize.setAttribute("class", "filesize");
+    filesize.textContent = convertFileSize(file.size);
+
+    div2.appendChild(filename);
+    div2.appendChild(filesize);
+    li.appendChild(div2);
+
+    let div3 = document.createElement("div");
+    div3.setAttribute("class", "col-md-2 status");
+
+    li.appendChild(div3);
+    filelist.appendChild(li);
 }
+
+let convertFileSize = x => {
+  var s = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'];
+  var e = Math.floor(Math.log(x) / Math.log(1024));
+  return (x / Math.pow(1024, e)).toFixed(2) + " " + s[e];
+};
 
