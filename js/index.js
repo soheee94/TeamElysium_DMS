69906da27075000c80efbe40357d17557c6fc24c
DESKTOP-QOUBLIC\hsh86 <hsh8616@naver.com>
2018-02-20 16:33:40 +0900

    add version management context menu

--- js/index.js
+++ js/index.js
@@ -126,6 +126,7 @@ let getDocumentList = id => {
                 list.setAttribute("onClick", "selectDocument(this.id)");
                 list.setAttribute("data-file", value.encodefilename);
                 list.setAttribute("data-c3code", value.c3_code);
+                list.setAttribute("class", "task");
         		list.textContent = value.name;
 
         		let downloadbtn = document.createElement("button");
@@ -138,6 +139,7 @@ let getDocumentList = id => {
         		list.appendChild(downloadbtn);
         		documentList.appendChild(list);
         	}
+
         },
         error: function(request, status, error) {
             console.log(request, status, error);
@@ -270,19 +272,23 @@ $("#upload").submit(function(e) {
 
                     if (dataStatus[i] === "존재하는 이름입니다.") {
                         $("#fileindex"+i).switchClass("primary", "danger"); 
-                        status.setAttribute("class", "fas fa-times statusicon");                      
-                    }
+                        status.setAttribute("class", "fas fa-times statusicon");
 
+                        let div4 = document.createElement("div");
+                        div4.setAttribute("class", "feedback");
+                        div4.textContent = "이미 존재하는 문서이름입니다.";
+
+                        document.getElementById("fileindex"+i).appendChild(div4);
+                    }
                     else{
                         $("#fileindex"+i).switchClass("primary", "success");
                         status.setAttribute("class", "fas fa-check statusicon");
                     }
-
                     status.style["margin-top"] = "8px";  
                     $("#fileindex"+i+" .statusicon").remove();
                     $("#fileindex"+i+" .status").append(status);
                 }
-                
+
                 return false;
             },
             cache: false,
@@ -412,3 +418,203 @@ let convertFileSize = x => {
   return (x / Math.pow(1024, e)).toFixed(2) + " " + s[e];
 };
 
+let versionManagementOpen = () => {
+    
+}
+
+
+////////////////////////////////////
+///context menu/////////////////////
+////////////////////////////////////
+function clickInsideElement( e, className ) {
+    var el = e.srcElement || e.target;
+    
+    if ( el.classList.contains(className) ) {
+      return el;
+    } else {
+      while ( el = el.parentNode ) {
+        if ( el.classList && el.classList.contains(className) ) {
+          return el;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  function getPosition(e) {
+    var posx = 0;
+    var posy = 0;
+
+    if (!e) var e = window.event;
+    
+    if (e.pageX || e.pageY) {
+      posx = e.pageX;
+      posy = e.pageY;
+    } else if (e.clientX || e.clientY) {
+      posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+      posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+
+    return {
+      x: posx,
+      y: posy
+    }
+  }
+
+
+  var contextMenuClassName = "context-menu";
+  var contextMenuItemClassName = "context-menu__item";
+  var contextMenuLinkClassName = "context-menu__link";
+  var contextMenuActive = "context-menu--active";
+
+  var taskItemClassName = "task";
+  var taskItemInContext;
+
+  var clickCoords;
+  var clickCoordsX;
+  var clickCoordsY;
+
+  var menu = document.querySelector("#context-menu");
+  var menuItems = menu.querySelectorAll(".context-menu__item");
+  var menuState = 0;
+  var menuWidth;
+  var menuHeight;
+  var menuPosition;
+  var menuPositionX;
+  var menuPositionY;
+
+  var windowWidth;
+  var windowHeight;
+
+  /**
+   * Initialise our application's code.
+   */
+  function initcontextmenu() {
+    contextListener();
+    clickListener();
+    keyupListener();
+    resizeListener();
+  }
+
+  /**
+   * Listens for contextmenu events.
+   */
+  function contextListener() {
+    document.addEventListener( "contextmenu", function(e) {
+      taskItemInContext = clickInsideElement( e, taskItemClassName );
+
+      if ( taskItemInContext ) {
+        e.preventDefault();
+        toggleMenuOn();
+        positionMenu(e);
+      } else {
+        taskItemInContext = null;
+        toggleMenuOff();
+      }
+    });
+  }
+
+  /**
+   * Listens for click events.
+   */
+  function clickListener() {
+    document.addEventListener( "click", function(e) {
+      var clickeElIsLink = clickInsideElement( e, contextMenuLinkClassName );
+
+      if ( clickeElIsLink ) {
+        e.preventDefault();
+        menuItemListener( clickeElIsLink );
+      } else {
+        var button = e.which || e.button;
+        if ( button === 1 ) {
+          toggleMenuOff();
+        }
+      }
+    });
+  }
+
+  /**
+   * Listens for keyup events.
+   */
+  function keyupListener() {
+    window.onkeyup = function(e) {
+      if ( e.keyCode === 27 ) {
+        toggleMenuOff();
+      }
+    }
+  }
+
+  /**
+   * Window resize event listener
+   */
+  function resizeListener() {
+    window.onresize = function(e) {
+      toggleMenuOff();
+    };
+  }
+
+  /**
+   * Turns the custom context menu on.
+   */
+  function toggleMenuOn() {
+    if ( menuState !== 1 ) {
+      menuState = 1;
+      menu.classList.add( contextMenuActive );
+    }
+  }
+
+  /**
+   * Turns the custom context menu off.
+   */
+  function toggleMenuOff() {
+    if ( menuState !== 0 ) {
+      menuState = 0;
+      menu.classList.remove( contextMenuActive );
+    }
+  }
+
+  /**
+   * Positions the menu properly.
+   * 
+   * @param {Object} e The event
+   */
+  function positionMenu(e) {
+    clickCoords = getPosition(e);
+    clickCoordsX = clickCoords.x;
+    clickCoordsY = clickCoords.y;
+
+    menuWidth = menu.offsetWidth + 4;
+    menuHeight = menu.offsetHeight + 4;
+
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+
+    if ( (windowWidth - clickCoordsX) < menuWidth ) {
+      menu.style.left = windowWidth - menuWidth + "px";
+    } else {
+      menu.style.left = clickCoordsX + "px";
+    }
+
+    if ( (windowHeight - clickCoordsY) < menuHeight ) {
+      menu.style.top = windowHeight - menuHeight + "px";
+    } else {
+      menu.style.top = clickCoordsY + "px";
+    }
+  }
+
+  /**
+   * Dummy action function that logs an action when a menu item link is clicked
+   * 
+   * @param {HTMLElement} link The link that was clicked
+   */
+  function menuItemListener( link ) {
+    console.log( "Task ID - " + taskItemInContext.getAttribute("id") + ", Task action - " + link.getAttribute("data-action"));
+    $("#versionManagementModal").modal();
+    toggleMenuOff();
+  }
+
+  /**
+   * Run the app.
+   */
+  initcontextmenu();
