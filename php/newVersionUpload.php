7a76c3a1203ea7ed3c2130d55b50041c5b191de6
DESKTOP-QOUBLIC\hsh86 <hsh8616@naver.com>
2018-02-22 15:23:42 +0900

    version upload exception : same file name

--- php/newVersionUpload.php
+++ php/newVersionUpload.php
@@ -21,8 +21,7 @@
 	$filename = $_FILES['file']['name'];
 
 	//[0] : code, [1] : name
-	$dbfilename = explode("_", $filename);
-
+	$filenameArray = explode("_", $filename);
 
 	//ftp login info
     $ftp_server = "igrus.mireene.com";  
@@ -35,21 +34,43 @@
 	// login with username and password
 	$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
 
+	// file exists check
+	$contents = ftp_nlist($conn_id, "/html/medical_document/");
+
+	function encoding($s1, $s2, $arr) { // 배열 전체 인코딩
+	    while (list($key, $val) = each($arr)) {
+	        $arr[$key] = iconv($s1, $s2, $val);
+	    }
+	}
+
+	encoding("UTF-8", "EUC-KR", $contents);
+
 	$ftpfilename = iconv("UTF-8", "EUC-KR", $filename);
+
+	if(in_array("/html/medical_document/".$ftpfilename, $contents)){
+		//[0]:filename [1]:filetype
+		$FtpfilenameExplode = explode(".", $ftpfilename);
+		$ftpfilename = $FtpfilenameExplode[0]."_1.".$FtpfilenameExplode[1];
+		$filenameArray[0] = $filenameArray[0]."_1";
+
+		$filenameExplode = explode(".", $filename);
+		$filename = $filenameExplode[0]."_1.".$filenameExplode[1];
+	}
+	
 	$tmp_file = $_FILES['file']['tmp_name'];
 	$remote_file = "/html/medical_document/". $ftpfilename;
 
 	// upload a file
 	if (ftp_put($conn_id, $remote_file, $tmp_file, FTP_BINARY)) {
-		$result = mysqli_query($connection,"INSERT INTO `dms_documentVersion`(`code`, `document_code`, `registrant`, `version`, `file`, `date`) VALUES ('".$dbfilename[0]."','".$document_code."','".$registrant."','0','".$filename."', now())");
+		$result = mysqli_query($connection,"INSERT INTO `dms_documentVersion`(`code`, `document_code`, `registrant`, `version`, `file`, `date`) VALUES ('".$filenameArray[0]."','".$document_code."','".$registrant."','0','".$filename."', now())");
 
 		if($result){
-			mysqli_query($connection, "UPDATE `dms_documentVersion` SET `version`= IF(`code` != '".$dbfilename[0]."', `version`+1 , `version`) WHERE `document_code` = '".$document_code."'");
+			mysqli_query($connection, "UPDATE `dms_documentVersion` SET `version`= IF(`code` != '".$filenameArray[0]."', `version`+1 , `version`) WHERE `document_code` = '".$document_code."'");
 			mysqli_query($connection, "UPDATE `dms_document` SET `filename`= '".$filename."' WHERE `code` = '".$document_code."'");
 		}
 
 		else{
-			echo "DB 등록 실패\n";
+			echo "Failed Insert DB\n";
 		}
 
 	 	
